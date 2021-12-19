732cf428ca829525ae939ff7471381e3d4a8608c
OlehPozharytskiy <pozhar.oleh25@gmail.com>
2021-12-19 11:35:07 +0200

    add build

---
+++ static/js/8.22de2ec8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpexam-third"]=this["webpackJsonpexam-third"]||[]).push([[8],{120:function(t,e,i){"use strict";i.r(e);i(1);var c=i(0);e.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)("h2",{children:"About"})})}}}]);
+//# sourceMappingURL=8.22de2ec8.chunk.js.map
